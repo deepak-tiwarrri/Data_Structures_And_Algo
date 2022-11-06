2556daf9d1855e5ae3fd15745c858b8ea8839328
Deepak483 <ainutiwari483@gmail.com>
2022-11-06 14:43:57 +0530

    nesting map set some line added

--- NestingMapSet.cpp
+++ NestingMapSet.cpp
@@ -23,21 +23,80 @@ int main()
         1 2
         d f 3
         2 3 4*/
-    map<pair<string, string>, vector<int>> m;
+    // map<pair<string, string>, vector<int>> m;
+    // int n;
+    // cin >> n;
+    // for (int i = 0; i < n; ++i)
+    // {
+    //     string fn, ln;
+    //     int ctn;
+    //     cin >> fn >> ln >> ctn;
+    //     for (int j = 0; j < ctn; ++j)
+    //     {
+    //         int x;
+    //         cin >> x;
+    //         m[{fn, ln}].push_back(x);
+    //     }
+    // }
+    // for (auto &pr : m)
+    // {
+    //     auto &mFirstPair = pr.first;
+    //     auto &mSecondPair = pr.second;
+    //     cout << "First Name and Second Name: " << endl;
+
+    //     cout << mFirstPair.first << " " << mFirstPair.second << endl;
+    //     cout << "Size of second Pair: " << mSecondPair.size() << endl;
+    //     for (auto &element : mSecondPair)
+    //     {
+    //         cout << element;
+    //     }
+    //     cout << endl;
+    // }
+
+    // map<pair<string, string>, vector<int>> m;
+    // m[{"deepak", "tiwari"}].push_back(4);
+    // m[{"sakshi", "tiwari"}].push_back(10);
+    // m[{"nishtha", "tiwari"}].push_back(30);
+    // auto it = m.begin();
+    // cout << it->first.first << endl;
+    // cout << it->first.second << endl;
+
+    /* the monk and the class Marks*/
+    /*  3
+        Eve 78
+        Bob 99
+        Alice 78
+
+        output:
+        Bob 99
+        Alice 78
+        Eve 78
+    */
+    map<int, set<string>> marks_map;
     int n;
     cin >> n;
     for (int i = 0; i < n; ++i)
     {
-        string fn, ln;
-        int ctn;
-        cin >> fn >> ln >> ctn;
-        for (int j = 0; j < ctn; ++j)
-        {
-            int x;
-            cin >> x;
-            m[{fn, ln}].push_back(x);
-        }
+        string name;
+        int marks;
+        cin >> name >> marks;
+        marks_map[-1 * marks].insert(name);
     }
-
+    // auto cur_it = --marks_map.end();
+    // while(true)
+    for (auto &marks_student_pr : marks_map)
+    {
+        // auto &students = cur_it->second;
+        auto &students = marks_student_pr.second;
+        // auto marks = cur_it->first;
+        auto marks = marks_student_pr.first;
+        for (auto student : students)
+        {
+            cout << student << " " << -1 * marks << endl;
+        }
+        // if (cur_it == marks_map.begin())
+        //     break;
+        // cur_it--;
+    }
     return 0;
 }
