d8bd4cc0231d4d1a9bf97ba93a58d1373ddf55a0
DeepakPC <ainutiwari483@gmail.com>
2022-11-08 14:25:45 +0530

    InbuiltSort and NextGreaterElement.cpp

---
+++ NextGreaterElement.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> NGE(vector<int> &v)
+{
+    vector<int> nge(v.size());
+    stack<int> st;
+    for (int i = 0; i < v.size(); ++i)
+    {
+        while (!st.empty() && v[i] > v[st.top()])
+        {
+            nge[st.top()] == i;
+        }
+        st.push(i);
+        st.pop();
+    }
+    while (!st.empty())
+    {
+        nge[st.top()] = -1;
+    }
+    return nge;
+}
+int main()
+{
+    // code here
+    /* 6
+       4 5 2 25 7 8
+    */
+    int n;
+    cin >> n;
+    vector<int> v(n); // vector of size n
+    for (auto i = 0; i < n; ++i)
+    {
+        cin >> v[i];
+    }
+    vector<int> nge = NGE(v);
+    for (int i = 0; i < n; ++i)
+    {
+        cout << v[i] << " " << (nge[i] == -1 ? -1 : v[nge[i]]) << endl;
+    }
+
+    return 0;
+}
