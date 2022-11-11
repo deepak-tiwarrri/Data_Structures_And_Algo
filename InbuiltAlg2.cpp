ae5dd66422f4bd9b1cff8b637f3f11741b134e15
DeepakPC <ainutiwari483@gmail.com>
2022-11-12 00:35:04 +0530

    Inbuilt Algo part 1 modified

---
+++ InbuiltAlg2.cpp
@@ -0,0 +1,12 @@
+// all_of,none_of,any_of
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // code here
+    // cout << [](int x)
+    // { return x + 2; }(2)
+    //      << endl;
+
+    return 0;
+}
