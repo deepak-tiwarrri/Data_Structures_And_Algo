8b1d0a699fd7c84a4aace2ca98bd5398748f2646
DeepakPC <ainutiwari483@gmail.com>
2024-10-04 22:25:28 +0530

    moveZeros

--- Array/moveZeros.cpp
+++ Array/moveZeros.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 void moveZeros(vector<int> &arr,int n){
    //create a temp array
-//   vector<int> temp;
-//   for(auto &it:arr){
-//    if(it!=0) temp.push_back(it);
-//   }
-//    //1 3 12
-//    int newSize = temp.size();
-//    for(int i=0;i<newSize;i++){
-//       arr[i] = temp[i];
-//    }
-//    for(int i=newSize;i<n;i++){
-//       arr[i] = 0;
-//    }
+  vector<int> temp;
+  for(auto &it:arr){
+   if(it!=0) temp.push_back(it);
+  }
+   //1 3 12
+   int newSize = temp.size();
+   for(int i=0;i<newSize;i++){
+      arr[i] = temp[i];
+   }
+   for(int i=newSize;i<n;i++){
+      arr[i] = 0;
+   }
 
-   //optimal approach
+   // optimal approach
    int j= -1;
    for(int i=0;i<n;i++){
       if(arr[i]==0){
@@ -29,6 +29,24 @@ void moveZeros(vector<int> &arr,int n){
          swap(arr[i],arr[j]);
          j++;
       }
+   }
+
+   //using while loop
+   int j= -1;
+   int i=0;
+   while(i<n){
+      if(arr[i]==0){
+         j = i;
+         break;
+      }
+   }
+   i = j+1;
+   while(i<n){
+      if(arr[i]!=0){
+         swap(arr[i],arr[j]);
+         j++;
+      }
+      i++;
    }
 
 }
