630ca11400d912be75c96c71f6d4b9cc8456bae1
DeepakPC <ainutiwari483@gmail.com>
2024-10-13 15:32:42 +0530

    missing array uncomment

--- Array/missingNumber.cpp
+++ Array/missingNumber.cpp
@@ -3,36 +3,36 @@ using namespace std;
 int missingNumber(vector<int> &nums){
     //go from i to nums.size() inclusive
     // brute force approach
-    // for(int i=0;i<=nums.size();++i){
-    //     bool flag = false;
-    //     for(int j=0;j<nums.size();++j){
-    //         // code goes here
-    //         if(nums[j]==i){
-    //             flag = true;
-    //             break;
-    //         }
-    //     }
-    //     if(flag == false) return i;
-    // }
-    // return -1;
+    for(int i=0;i<=nums.size();++i){
+        bool flag = false;
+        for(int j=0;j<nums.size();++j){
+            // code goes here
+            if(nums[j]==i){
+                flag = true;
+                break;
+            }
+        }
+        if(flag == false) return i;
+    }
+    return -1;
     //second appraoch using hashing SC:O(n+1)
-    // int n = nums.size();
-    // int freqHash[n+1] = {0};
-    // for(int i=0;i<n;i++){
-    //     freqHash[nums[i]]++;
-    // }
-    // for (int i = 0; i <=n; i++)
-    // {   
-    //     if(freqHash[i]==0) return i;
-    // }
-    // return -1;
+    int n = nums.size();
+    int freqHash[n+1] = {0};
+    for(int i=0;i<n;i++){
+        freqHash[nums[i]]++;
+    }
+    for (int i = 0; i <=n; i++)
+    {   
+        if(freqHash[i]==0) return i;
+    }
+    return -1;
      int n = nums.size();
-    // long long sumOfN = (n*(n+1)/2);
-    // long long sumTillN = 0;
-    // for(int i=0;i<n;++i){
-    //     sumTillN+= nums[i];
-    // }
-    // return static_cast<int>(sumOfN-sumTillN);
+    long long sumOfN = (n*(n+1)/2);
+    long long sumTillN = 0;
+    for(int i=0;i<n;++i){
+        sumTillN+= nums[i];
+    }
+    return static_cast<int>(sumOfN-sumTillN);
     int xor1 = 0,xor2=0;
     for(int i=0;i<nums.size();i++){
         // xor1 = xor1^i;//both line are right
