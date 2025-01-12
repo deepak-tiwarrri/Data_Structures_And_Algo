5f329bb4309b78dccd8b5a115dd9993e0e62221c
DeepakPC <ainutiwari483@gmail.com>
2025-01-12 16:15:19 +0530

    boundary traversasl

--- BinaryTree/checkIfBTBalanced.cpp
+++ BinaryTree/checkIfBTBalanced.cpp
@@ -9,26 +9,27 @@ public:
     
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
-
-bool isBalanced(TreeNode* root){
-    if(!root) return 0;
-    int lh = isBalanced(root->left);
-    int rh = isBalanced(root->right);
+int findHeight(TreeNode* root){
+     if(!root) return 0;
+    int lh = findHeight(root->left);
+    int rh = findHeight(root->right);
     if(lh==-1 || rh==-1) return -1;
     if(abs(lh-rh)>1) return -1;
     return 1+max(lh,rh);
+}
+bool isBalanced(TreeNode* root){
+   return findHeight(root)!=-1;
 }
 
 int main() {
     // code here
     TreeNode* root = new TreeNode(1);
     root->left = new TreeNode(2);
-    root->right = new TreeNode(3);
-    root->left->left = new TreeNode(4);
-    root->left->right = new TreeNode(9);
-    root->right->left = new TreeNode(9);
-    root->right->left->right = new TreeNode(10);
-    root->left->left->right = new TreeNode(5);
+    root->right = new TreeNode(2);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(3);
+    root->left->left->left = new TreeNode(4);
+    root->left->left->right = new TreeNode(4);
     bool isbalanced = isBalanced(root);
     if(isbalanced) cout<<"true"<<endl;
     else cout<<"false"<<endl;   
