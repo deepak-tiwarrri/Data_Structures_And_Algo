772b2c96637d7d0012a7ed9ff27a922b3f80de5c
DeepakPC <ainutiwari483@gmail.com>
2025-01-16 00:09:06 +0530

    morris Traversal

--- BinaryTree/boundaryTraversal.cpp
+++ BinaryTree/boundaryTraversal.cpp
@@ -1,72 +1,94 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class TreeNode {
+class TreeNode
+{
 public:
-    int data;
-    TreeNode* left;
-    TreeNode* right;
-    
-    TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+   int data;
+   TreeNode *left;
+   TreeNode *right;
+
+   TreeNode(int x) : data(x), left(NULL), right(NULL) {}
 };
-bool isLeaf(TreeNode* root){
-    if(root->left==nullptr && root->right==nullptr) return true;
-    else return false; 
+bool isLeaf(TreeNode *root)
+{
+   if (root->left == nullptr && root->right == nullptr)
+      return true;
+   else
+      return false;
 }
-void addLeftBoundary(TreeNode* root,vector<int> &res){
-    //start from the left
-    TreeNode* curr = root->left;
-    while(curr){
-        if(!isLeaf(root)) res.push_back(curr->data);
-        if(curr->left) curr = curr->left;
-        else curr = curr->right;
-    }
+void addLeftBoundary(TreeNode *root, vector<int> &res)
+{
+   // start from the left
+   TreeNode *curr = root->left;
+   while (curr)
+   {
+      if (!isLeaf(root))
+         res.push_back(curr->data);
+      if (curr->left)
+         curr = curr->left;
+      else
+         curr = curr->right;
+   }
 }
-void addLeavesNodes(TreeNode* root,vector<int> &res){
-    //use preorder traversal root left right
-    if(isLeaf(root)) res.push_back(root->data);
-    //move as left as possible
-    if(root->left){
-        addLeavesNodes(root->left,res);
-    }
-    else{ 
-        addLeavesNodes(root->right,res);
-    }
+void addLeavesNodes(TreeNode *root, vector<int> &res)
+{
+   // use preorder traversal root left right
+   if (isLeaf(root))
+      res.push_back(root->data);
+   // move as left as possible
+   if (root->left)
+   {
+      addLeavesNodes(root->left, res);
+   }
+   else
+   {
+      addLeavesNodes(root->right, res);
+   }
 }
-void addRightBoundary(TreeNode* root,vector<int> &res){
-    //make temp array add the value and then store in res in reverse order
-    vector<int> temp;
-    TreeNode* curr = root->right;
-    while(curr){
-        if(!isLeaf(root)) temp.push_back(curr->data);
-        if(curr->right) curr = curr->right;
-        else curr = curr->left;
-    }
+void addRightBoundary(TreeNode *root, vector<int> &res)
+{
+   // make temp array add the value and then store in res in reverse order
+   vector<int> temp;
+   TreeNode *curr = root->right;
+   while (curr)
+   {
+      if (!isLeaf(root))
+         temp.push_back(curr->data);
+      if (curr->right)
+         curr = curr->right;
+      else
+         curr = curr->left;
+   }
+}
+vector<int> boundary(TreeNode *root)
+{
+   // first push the first root node in ans
+   vector<int> res;
+   if (root == NULL)
+      return res;
+   if (!isLeaf(root))
+      res.push_back(root->data);
+   addLeftBoundary(root, res);
+   addLeavesNodes(root, res);
+   addRightBoundary(root, res);
+   return res;
+}
+int main()
+{
+   // code here
+   TreeNode *root = new TreeNode(1);
+   root->left = new TreeNode(2);
+   root->right = new TreeNode(2);
+   root->left->left = new TreeNode(3);
+   root->left->right = new TreeNode(3);
+   root->left->left->left = new TreeNode(4);
+   root->left->left->right = new TreeNode(4);
+   vector<int> res = boundary(root);
+   for (int i = 0; i < res.size(); i++)
+   {
+      cout << res[i] << " " << endl;
+   }
 
-}
-vector<int> boundary(TreeNode* root){
-    //first push the first root node in ans
-    vector<int> res;
-    if(root==NULL) return res;
-    if(!isLeaf(root)) res.push_back(root->data);
-    addLeftBoundary(root,res);
-    addLeavesNodes(root,res);
-    addRightBoundary(root,res);
-    return res;
-}
-int main() {
-    // code here
-    TreeNode* root = new TreeNode(1);
-    root->left = new TreeNode(2);
-    root->right = new TreeNode(2);
-    root->left->left = new TreeNode(3);
-    root->left->right = new TreeNode(3);
-    root->left->left->left = new TreeNode(4);
-    root->left->left->right = new TreeNode(4);
-    vector<int> res = boundary(root);
-    for(int i=0;i<res.size();i++){
-        cout<<res[i]<<" "<<endl;
-    }
-
-    return 0;
+   return 0;
 }
---
+++ BinaryTree/morrisTraversal.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+class TreeNode
+{
+
+public:
+   int data;
+   TreeNode *left;
+   TreeNode *right;
+
+   TreeNode(int data1, TreeNode *left1, TreeNode *right1)
+   {
+      data = data1;
+      left = left1;
+      right = right1;
+   }
+   TreeNode(int data1)
+   {
+      data = data1;
+      left = nullptr;
+      right = nullptr;
+   }
+};
+vector<int> morrisTraversal(TreeNode *root)
+{
+   // morris traversal didn't take any space
+   vector<int> ans;
+   if (root == nullptr)
+      return ans;
+   TreeNode *curr = root;
+   while (curr != nullptr)
+   {
+      if (curr->left == nullptr)
+      {
+         // then push back and
+         ans.push_back(curr->data);
+         curr = curr->right;
+      }
+      else
+      {
+         // take the left and point the thread
+         TreeNode *prevNode = curr->left;
+         while (prevNode->right != nullptr && prevNode->right != curr)
+         {
+            prevNode = prevNode->right;
+         }
+         // if prevNode->right == nullptr
+         if (prevNode->right == nullptr)
+         {
+            prevNode->right = curr;
+            curr = curr->left;
+         }
+         else
+         {
+            prevNode->right = nullptr;
+            ans.push_back(curr->data);
+            curr = curr->right;
+         }
+      }
+   }
+   return ans;
+}
+int main()
+{
+   TreeNode *root = new TreeNode(1);
+   root->left = new TreeNode(2);
+   root->right = new TreeNode(3);
+   root->left->left = new TreeNode(4);
+   root->left->right = new TreeNode(5);
+   root->right->left = new TreeNode(6);
+   root->right->right = new TreeNode(7);
+
+   vector<int> result = morrisTraversal(root);
+
+   for (int val : result)
+   {
+      cout << val << " ";
+   }
+   return 0;
+}
