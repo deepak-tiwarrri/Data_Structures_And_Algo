dec36ec31d31e0188399b39d09272765697f07d5
DeepakPC <ainutiwari483@gmail.com>
2025-06-09 23:22:28 +0530

    made changes in remove duplicates

--- Array/removeDuplicates.cpp
+++ Array/removeDuplicates.cpp
@@ -1,44 +1,74 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int removeDuplicates(vector<int> &arr,int n){
-    //1,1,2,3,3,4,5,5,5
-    // set<int> st;
-    // for(auto &it:arr){
-    //     st.insert(it);
+int removeDuplicates(vector<int> &arr, int n)
+{
+    // 1,1,2,3,3,4,5,5,5
+    // set<int> uniqueNoSet;
+    // for (auto &it : arr)
+    // {
+    //     uniqueNoSet.insert(it);
     // }
     // int index = 0;
-    // //1,2,3,4,5
-    // for(auto &val:st){
-    //     arr[index] = val;
+    // for (auto &ele : uniqueNoSet)
+    // {
+    //     arr[index] = ele;
     //     index++;
     // }
     // return index;
-    int i=0;
-    for(int j=1;j<n;j++){
-        if(arr[j]!=arr[i]){
-            arr[i+1] = arr[j];
+
+    int i = 0, j = i + 1;
+    while (j < n)
+    {
+        if (arr[i] != arr[j])
+        {
             i++;
+            arr[i] = arr[j];
+        }
+        else
+        {
+            j++;
         }
     }
-    return i+1;
+    return i + 1;
+    // int i=0;
+    // for(int j=1;j<n;j++){
+    //     if(arr[j]!=arr[i]){
+    //         arr[i+1] = arr[j];
+    //         i++;
+    //     }
+    // }
+    // return i+1;
 }
-void leftRotateArrayOne(vector<int> &arr,int n){
+void leftRotateArrayOne(vector<int> &arr, int n)
+{
     int temp = arr[0];
-    for(int i=1;i<n;i++){
-        arr[i-1] = arr[i];
+    for (int i = 1; i < n; i++)
+    {
+        arr[i - 1] = arr[i];
     }
-    arr[n-1] = temp;
+    arr[n - 1] = temp;
 }
 
-int main(){
-    
-    vector<int> v1 = {1,1,2,3,3,4,5,5,5};
-    // int result = removeDuplicates(v1,v1.size());
-    // cout<<result<<endl;
-    leftRotateArrayOne(v1,v1.size());
-    for(auto &it:v1){
-        cout<<it<<" ";
+int main()
+{
+
+    int n;
+    cin >> n;
+    vector<int> nums;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        nums.push_back(x);
+    }
+
+    int result = removeDuplicates(nums, n);
+    cout << result << endl;
+    // leftRotateArrayOne(v1,v1.size());
+    for (auto &it : nums)
+    {
+        cout << it << " ";
     }
     return 0;
 }
