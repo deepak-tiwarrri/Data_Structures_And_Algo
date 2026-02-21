d1a65ac3dd04071a520a6292d70d3b82062eb5cb
Deepak Tiwari <tiwarideepak8707@gmail.com>
2026-02-21 22:55:55 +0530

    feat: implement lowest common ancestor function for binary tree and root to node path in binary tree

---
+++ BinaryTree/lowestCommonAncestor.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+class TreeNode
+{
+public:
+   int data;
+   TreeNode *left;
+   TreeNode *right;
+
+   TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+};
+TreeNode *lowestCommmonAncestor(TreeNode *p, TreeNode *q, TreeNode *root)
+{
+   if (root == NULL || root == p || root == q)
+      return root;
+
+   TreeNode *left = lowestCommmonAncestor(p, q, root->left);
+   TreeNode *right = lowestCommmonAncestor(p, q, root->right);
+
+   if (left == nullptr)
+   {
+      return right;
+   }
+   else if (right == nullptr)
+   {
+      return left;
+   }
+   else
+   {
+      return root;
+   }
+}
+int main()
+{
+   // code here
+   TreeNode *root = new TreeNode(1);
+   root->left = new TreeNode(2);
+   root->right = new TreeNode(2);
+   root->left->left = new TreeNode(3);
+   root->left->right = new TreeNode(3);
+   root->left->left->left = new TreeNode(4);
+   root->left->left->right = new TreeNode(4);
+   TreeNode *res = lowestCommmonAncestor();
+   cout << res->data << endl;
+   cout << endl;
+   return 0;
+}
--- BinaryTree/rootToNodePath.cpp
+++ BinaryTree/rootToNodePath.cpp
@@ -1,73 +1,116 @@
 #include <bits/stdc++.h>
 using namespace std;
-class TreeNode{
-    public:
+class TreeNode
+{
+public:
     int data;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(int data1, TreeNode *left1, TreeNode *right1){
+    TreeNode(int data1, TreeNode *left1, TreeNode *right1)
+    {
         data = data1;
         left = left1;
         right = right1;
     }
-    TreeNode(int data1){
+    TreeNode(int data1)
+    {
         data = data1;
         left = nullptr;
         right = nullptr;
     }
 };
-//code to find the root to path node all if the node is present 
-bool isPathAvailable(TreeNode* root,vector<int> &path,vector<vector<int>> &allPath,int x){
-    if(!root) return false;
+bool rootToNodePath(TreeNode *root, vector<int> &ans, int &node)
+{
+    if (root == nullptr)
+        return false;
+    ans.push_back(root->data);
+    if (node == root->data)
+    {
+        return true;
+    }
+    if (rootToNodePath(root->left, ans, node) == true)
+    {
+        return true;
+    }
+    if (rootToNodePath(root->right, ans, node) == true)
+        return true;
+
+    ans.pop_back();
+    return false;
+}
+vector<int> findNodeToRootPath(TreeNode *root, int node)
+{
+    vector<int> ans;
+    if (root == nullptr)
+        return ans;
+    rootToNodePath(root, ans, node);
+    return ans;
+}
+// code to find the root to path node all if the node is present
+bool isPathAvailable(TreeNode *root, vector<int> &path, vector<vector<int>> &allPath, int x)
+{
+    if (!root)
+        return false;
 
     path.push_back(root->data);
     // if(root->data==x) return true;//this will return the true and will only give one path node
 
-    if(root->left!=nullptr && root->right!=nullptr){
+    if (root->left != nullptr && root->right != nullptr)
+    {
         allPath.push_back(path);
-    }else{
-        //if root->left is false and root->right is false
-        //recursively call that
-        isPathAvailable(root->left,path,allPath,x);
-        isPathAvailable(root->right,path,allPath,x);
+    }
+    else
+    {
+        // if root->left is false and root->right is false
+        // recursively call that
+        isPathAvailable(root->left, path, allPath, x);
+        isPathAvailable(root->right, path, allPath, x);
     }
     path.pop_back();
     return false;
 }
-void recursion(TreeNode* root,vector<int> &path,vector<vector<int>> &allPath){
-    if(!root) return;
+void recursion(TreeNode *root, vector<int> &path, vector<vector<int>> &allPath)
+{
+    if (!root)
+        return;
     path.push_back(root->data);
-    if(root->left==nullptr && root->right==nullptr){
+    if (root->left == nullptr && root->right == nullptr)
+    {
         allPath.push_back(path);
     }
-    recursion(root->left,path,allPath);
-    recursion(root->right,path,allPath);
+    recursion(root->left, path, allPath);
+    recursion(root->right, path, allPath);
     path.pop_back();
 }
-vector<vector<int>> allRootToLeaf(TreeNode* root) {
-            //your code goes here
+vector<vector<int>> allRootToLeaf(TreeNode *root)
+{
+    // your code goes here
     vector<int> path;
     vector<vector<int>> allPath;
-    if(root==nullptr) return allPath;
+    if (root == nullptr)
+        return allPath;
     // isPathAvailable(root,path,x);
-    recursion(root,path,allPath);
+    recursion(root, path, allPath);
     return allPath;
 }
-int main() {
+int main()
+{
     // code here
-    TreeNode* root = new TreeNode(2);
+    TreeNode *root = new TreeNode(2);
     root->left = new TreeNode(3);
     root->right = new TreeNode(5);
     root->left->right = new TreeNode(9);
-    root->left->left  =new TreeNode(6);
+    root->left->left = new TreeNode(6);
     root->left->right->right = new TreeNode(7);
     root->left->right->left = new TreeNode(10);
     vector<vector<int>> res = allRootToLeaf(root);
-    for(auto &ele:res){
-        for(auto &it:ele){
-            cout<<it<<" ";
+    for (auto &ele : res)
+    {
+        for (auto &it : ele)
+        {
+            cout << it << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
     return 0;
