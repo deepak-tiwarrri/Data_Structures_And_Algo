2ad0433f0b4ac239cd313261b4ba4364347b5c02
Deepak Tiwari <tiwarideepak8707@gmail.com>
2026-03-24 23:12:52 +0530

    changed: bstIterator and twosum bst

--- BinaryTree/bstIterator.cpp
+++ BinaryTree/bstIterator.cpp
@@ -1,79 +1,64 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int data;
- *     TreeNode *left;
- *     TreeNode *right;
- *      TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
- * };
- **/
 #include <bits/stdc++.h>
 using namespace std;
 class TreeNode
 {
 public:
-  int data;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode(int val) : data(val), left(nullptr), right(nullptr) {}
+   int data;
+   TreeNode *right;
+   TreeNode *left;
+   TreeNode(int data, TreeNode *left = nullptr, TreeNode *right = nullptr)
+   {
+      this->data = data;
+      this->left = left;
+      this->right = right;
+   }
 };
-class BSTIterator
+class Solutions
 {
 public:
-  stack<TreeNode *> st;
-  BSTIterator(TreeNode *root)
-  {
-    pushAll(root);
-  }
+   stack<TreeNode *> st;
 
-  bool hasNext()
-  {
-    return !st.empty();
-  }
+   Solutions(TreeNode *root)
+   {
+      pushAll(root);
+   }
+   int next()
+   {
+      TreeNode *node = st.top();
+      st.pop();
+      if (node->right != nullptr)
+         pushAll(node->right);
+      return node->data;
+   }
+   bool hasNext()
+   {
+      return !st.empty();
+   }
 
-  int next()
-  {
-    TreeNode *node = st.top();
-    st.pop();
-    int data = node->data;
-    pushAll(node->right);
-    return data;
-  }
-  private:
-  void pushAll(TreeNode* root){
-    while(root!=nullptr){
-      st.push(root);
-      root = root->left;
-    }
-  }
+private:
+   void pushAll(TreeNode *root)
+   {
+      while (root != nullptr)
+      {
+         st.push(root);
+         root = root->left;
+      }
+   }
 };
 
 int main()
 {
-  BSTIterator bSTIterator = new BSTIterator([ 7, 3, 15, null, null, 9, 20 ]);
-  bSTIterator.next(); // return 3
-
-  bSTIterator.next(); // return 7
-
-  bSTIterator.hasNext(); // return True
-
-  bSTIterator.next(); // return 9
-
-  bSTIterator.hasNext(); // return True
-
-  bSTIterator.next(); // return 15
-
-  bSTIterator.hasNext(); // return True
-
-  bSTIterator.next(); // return 20
-
-  bSTIterator.hasNext(); // return False
-  return 0;
+   // code here
+   TreeNode *root = new TreeNode(7);
+   root->left = new TreeNode(3);
+   root->right = new TreeNode(15);
+   root->right->left = new TreeNode(9);
+   root->right->right = new TreeNode(20);
+   Solutions *sol = new Solutions(root);
+   sol->next();
+   sol->hasNext();
+   sol->next();
+   sol->next();
+   sol->hasNext();
+   return 0;
 }
-
-/**
- * Your BSTIterator object will be instantiated and called as such:
- * BSTIterator* obj = new BSTIterator(root);
- * int param_1 = obj->next();
- * bool param_2 = obj->hasNext();
- */
